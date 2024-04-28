8513fdf6c94b106c187fd85610c72f1e53c9d72a
Smart Agwu <sagwu@hubspot.com>
2024-04-28 17:29:23 +0100

    implement code mirror editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import "./App.css";
+import CodeEditor from "./components/CodeEditor";
+
+const DEFAULT_HTML = "<h1>My code editor HTML</h1>";
 
 function App() {
-  return <div className="App">HTML Editor</div>;
+  const [code, setCode] = useState(DEFAULT_HTML);
+  const handleOnChange = useCallback(
+    (value: string) => {
+      console.info(code);
+      setCode(value);
+    },
+    [code],
+  );
+
+  return (
+    <div className="App">
+      <CodeEditor defaultCode={DEFAULT_HTML} handleOnChange={handleOnChange} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,33 @@
+import { html } from "@codemirror/lang-html";
+import { githubDark } from "@uiw/codemirror-theme-github";
+import { useCodeMirror } from "@uiw/react-codemirror";
+import { useRef } from "react";
+
+const CodeEditor = ({
+  defaultCode,
+  handleOnChange,
+}: {
+  defaultCode: string;
+  handleOnChange: (value: string) => void;
+}) => {
+  const editor = useRef(null);
+
+  useCodeMirror({
+    value: defaultCode,
+    container: editor.current,
+    extensions: [
+      {
+        extension: [html()],
+      },
+    ],
+    onChange: handleOnChange,
+    theme: {
+      extension: [githubDark],
+    },
+    minHeight: "300px",
+  });
+
+  return <div ref={editor} />;
+};
+
+export default CodeEditor;
